7d2e70137e988bcbcc949737369e961ca06ab15c
007db <1907132646@qq.com>
2026-08-07 10:30:00 +0800

    Update admin-login.js

--- functions/api/admin-login.js
+++ functions/api/admin-login.js
@@ -1,4 +1,79 @@
-export async function onRequestPost(context){
-const d=await context.request.json();
-return Response.json({success:d.username==="admin"&&d.password==="admin123",role:"super_admin"});
+export async function onRequestPost({request,env}){
+
+
+try{
+
+
+const {
+username,
+password
+}=await request.json();
+
+
+
+const admin =
+await env.DB.prepare(
+
+`
+SELECT *
+FROM admins
+WHERE username=?
+AND password=?
+`
+
+)
+.bind(
+username,
+password
+)
+.first();
+
+
+
+if(!admin){
+
+return Response.json({
+
+success:false,
+
+message:"账号或密码错误"
+
+});
+
+}
+
+
+
+return Response.json({
+
+success:true,
+
+user:{
+username:admin.username,
+role:admin.role
+}
+
+});
+
+
+}
+
+
+catch(error){
+
+
+return Response.json({
+
+success:false,
+
+error:error.message
+
+},{
+status:500
+});
+
+
+}
+
+
 }
