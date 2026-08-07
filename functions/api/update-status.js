fa52a2b52f6f26acf66bf64c08c33eb5c353bf23
007db <1907132646@qq.com>
2026-08-07 11:09:10 +0800

    Update update-status.js

--- functions/api/update-status.js
+++ functions/api/update-status.js
@@ -1,1 +1,37 @@
+export async function onRequestPost({request,env}){
 
+
+const data =
+await request.json();
+
+
+
+await env.DB.prepare(
+
+`
+UPDATE appointments
+SET status=?
+WHERE id=?
+
+`
+
+)
+.bind(
+
+data.status,
+
+data.id
+
+)
+.run();
+
+
+
+return Response.json({
+
+success:true
+
+});
+
+
+}
