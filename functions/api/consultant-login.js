ff0e0769d89dad949f177f558eb2704628f2bc4a
007db <1907132646@qq.com>
2026-08-07 12:51:57 +0800

    Update consultant-login.js

--- functions/api/consultant-login.js
+++ functions/api/consultant-login.js
@@ -1,90 +1,11 @@
-export async function onRequestPost({request,env}){
-
-
-try{
-
-
-const {
-username,
-password
-}=await request.json();
-
-
-
-const user =
-await env.DB.prepare(
-
-`
-SELECT 
-id,
-username,
-name,
-role,
-phone
-
-FROM users
-
-WHERE username=?
-AND password=?
-AND role='consultant'
-
-`
-
-)
-.bind(
-
-username,
-
-password
-
-)
-.first();
-
-
-
-if(!user){
-
-
-return Response.json({
-
-success:false,
-
-message:"账号或密码错误"
-
-});
-
-
-}
-
-
+export async function onRequestPost(){
 
 return Response.json({
 
 success:true,
 
-user:user
+message:"咨询师API正常"
 
 });
-
-
-
-}
-
-catch(error){
-
-
-return Response.json({
-
-success:false,
-
-message:error.message
-
-},{
-status:500
-});
-
-
-}
-
 
 }
